ca0b27a4c2b82114da7b9e87f533ac85a0212f61
Leozin89python <leozin069@gmail.com>
2021-07-30 01:21:14 -0300

    full-stack-jr

--- frontend/auth/index.js
+++ frontend/auth/index.js
@@ -1,8 +1,32 @@
-document.onclick = addEventListener('click',(e) => e.preventDefault())
-document.onload = addEventListener('load',()=>{
+document.onclick = addEventListener('click',(e)=> e.preventDefault())
+document.onload  = addEventListener('load', ( )=>{
     $('#alerting').hide(1)
     $('#empty').hide(1)
 })  
 $('#button').click(function(){
-    document.location.href = 'web/index.html'
+    let admInput  = document.querySelector('#admin')
+    let passInput = document.querySelector('#pass')
+
+    let adm  = 'mendes'
+    let pass = '1234'
+
+    if(
+            admInput.value  === ''
+        ||
+            passInput.value === ''
+    ){
+        $('#empty').show(100)
+    }else if(
+            admInput.value  != adm
+        &&
+            passInput.value != pass
+    ){
+        $('#alerting').show(100) 
+    }else if(
+             admInput.value  === adm
+        &&
+            passInput.value  === pass
+    ){
+        document.location.href = 'web/index.html'
+    }
 })
