46af42966d2f79eb89f5d89b648cd19fad1e7953
Leozin89python <leozin069@gmail.com>
2021-08-01 19:56:11 -0300

    full-stack-jr

--- frontend/admin/index.js
+++ frontend/admin/index.js
@@ -1,14 +1,26 @@
 let doc = document
-doc.onload = addEventListener('loa',()=>{
+doc.onload = addEventListener('load',()=>{
     const url = 'category.json'
-
-    const config = {
-        method:'GET'
-    }
-
-    fetch(url).then(res => console.log(res.json()))
+    teste()
 })
 doc.onclick = addEventListener('click',e => e.preventDefault())
 $('#homeArea').click(function(){
    doc.location.href = '../web/index.html'
 })
+function teste(){
+    const url = 'category.json'
+    const select = document.querySelector('#inputGroupSelect01')
+
+    fetch(url)
+        .then(res => res.json())
+        .then(res => {
+            res.map(e => {
+                let category = e.category
+                let option   = document.createElement('option')
+                
+                option.append(category)
+                select.append(option)
+            })
+        })
+        .catch(err => err)
+}
