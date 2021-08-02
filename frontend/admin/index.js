92f90dec5860f14840bfb49fe8469be987660d6b
Leozin89python <leozin069@gmail.com>
2021-08-01 21:21:15 -0300

    full-stack-jr

--- frontend/admin/index.js
+++ frontend/admin/index.js
@@ -24,18 +24,5 @@ function sector(){
         .catch(err => console.log(err))
 }
 $('#send').click(function(){
-    const url = 'category.json'
-    const area = document.querySelector('#dataCat')
-
-    fetch(url)
-            .then(res => res.json())
-            .then(res => {
-                res.map(e => {
-                    let cat = e.category
-                    let ul  = document.createElement('ul')
-                    ul.append(cat)
-                    area.append(ul)
-                })
-            })
-            .catch(e => console.log(e))
+   alert('sending ...')
 })
