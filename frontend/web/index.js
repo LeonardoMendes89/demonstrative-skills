2e405ca469aa3b56785668935a16bf18abc717d2
Leozin89python <leozin069@gmail.com>
2021-08-03 18:19:51 -0300

    full-stack-jr

--- frontend/web/index.js
+++ frontend/web/index.js
@@ -2,6 +2,7 @@ let doc = document
 document.onload = addEventListener('load',()=>{
     $('#alert').hide(1)
     $('#noDataTable').hide(1)
+    $('#dataSearched').hide(1)
     dataTable()
 })
 $('.sair').click(function(){
@@ -64,3 +65,118 @@ function dataTable(){
                     $('#noDataTable').show(100)
                 })
 }
+$('#search').keypress(function(){
+    
+    $('#dataStandard').hide()
+    $('#dataSearched').show()
+
+    let id      = document.querySelector('#idDbSc')
+    let name    = document.querySelector('#nameDbSC')
+    let job     = document.querySelector('#jobDbSC')
+    let salary  = document.querySelector('#salaryDbSC')
+    let sector  = document.querySelector('#sectorDbSC')
+
+    let search  = document.querySelector('#search').value
+    //alert(search)
+
+    const url = `http://localhost:3003`
+
+    const config = {
+        method :'GET'
+    }
+
+    fetch(url,config)
+                .then(res => res.json())
+                .then(res => {
+                    res.map(e => {
+
+                        if(search === e.name){
+
+                            let idDb = e.id
+                            let ulId = doc.createElement('ul')
+                            ulId.append(idDb)
+                            id.append(ulId)
+    
+                             let nameDb = e.name.substring(0,15)
+                             let ulName = doc.createElement('ul')
+                             ulName.append(nameDb)
+                             name.append(ulName)
+    
+                             let jobDb = e.job.substring(0,15)
+                             let ulJob = doc.createElement('ul')
+                             ulJob.append(jobDb)
+                             job.append(ulJob)
+    
+                             let salaryDb = e.salary
+                             let ulSalary = doc.createElement('ul')
+                             ulSalary.append(salaryDb)
+                             salary.append(ulSalary)
+    
+                             let sectorDb = e.sector
+                             let ulSector = doc.createElement('ul')
+                             ulSector.append(sectorDb)
+                             sector.append(ulSector)
+
+                        }else{
+                            return []
+                        }
+
+                    })
+                })
+                .catch(_ => {
+                    $('#noDataTable').show(100)
+                })
+})
+$('#search').keydown(function(){
+    $('#dataStandard').show()
+    $('#dataSearched').hide()
+})  
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+                        let idDb = e.id
+                        let ulId = doc.createElement('ul')
+                        ulId.append(idDb)
+                        id.append(ulId)
+
+                         let nameDb = e.name.substring(0,15)
+                         let ulName = doc.createElement('ul')
+                         ulName.append(nameDb)
+                         name.append(ulName)
+
+                         let jobDb = e.job.substring(0,15)
+                         let ulJob = doc.createElement('ul')
+                         ulJob.append(jobDb)
+                         job.append(ulJob)
+
+                         let salaryDb = e.salary
+                         let ulSalary = doc.createElement('ul')
+                         ulSalary.append(salaryDb)
+                         salary.append(ulSalary)
+
+                         let sectorDb = e.sector
+                         let ulSector = doc.createElement('ul')
+                         ulSector.append(sectorDb)
+                         sector.append(ulSector)
+
+                         $('#dataStandard').hide(1)* 
+ * 
+ * 
+
+
+
+
+
+
+
+
+
+
+
+
+ */
